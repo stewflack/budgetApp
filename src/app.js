5ce6a2dab08ef3f256cef9e0774b52982fb5c023
Stewart <swflack@gmail.com>
2020-04-28 00:08:00 +0100

    Heroku first commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+const express = require('express')
+const hbs = require('hbs')
+
+/**
+ * Express Set Up
+ * Ports
+ */
+const app = express()
+const port = process.env.PORT || 3000
+
+// Paths for express config
+const publicDirPath = path.join(__dirname, '../dist')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup Handlebars engine and Views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+// hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.sendFile('../dist/index.html')
+})
+app.listen(port,() => {
+    console.log(`Server has started... port: ${port}`)
+})
