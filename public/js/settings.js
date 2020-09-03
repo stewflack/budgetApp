710720b32e1a6e68ab3541395028e5fb0a0457f3
stewartflack <stewart.flack@verseone.com>
2020-09-03 08:07:08 +0100

    using a colour picker library to determine the colour of a type. Pretty good usage so far

--- public/js/settings.js
+++ public/js/settings.js
@@ -63,6 +63,73 @@ const settingController = () => {
             removeParentFromUI(element);
         }
     }
+    const pickr = Pickr.create({
+        el: '.color-picker',
+        theme: 'nano', // or 'monolith', or 'nano'
+    
+        swatches: [
+            'rgba(244, 67, 54, 1)',
+            'rgba(233, 30, 99, 0.95)',
+            'rgba(156, 39, 176, 0.9)',
+            'rgba(103, 58, 183, 0.85)',
+            'rgba(63, 81, 181, 0.8)',
+            'rgba(33, 150, 243, 0.75)',
+            'rgba(3, 169, 244, 0.7)',
+            'rgba(0, 188, 212, 0.7)',
+            'rgba(0, 150, 136, 0.75)',
+            'rgba(76, 175, 80, 0.8)',
+            'rgba(139, 195, 74, 0.85)',
+            'rgba(205, 220, 57, 0.9)',
+            'rgba(255, 235, 59, 0.95)',
+            'rgba(255, 193, 7, 1)'
+        ],
+    
+        components: {
+    
+            // Main components
+            preview: true,
+            opacity: true,
+            hue: true,
+    
+            // Input / output Options
+            interaction: {
+                hex: true,
+                rgba: true,
+                hsla: true,
+                hsva: true,
+                cmyk: true,
+                input: true,
+                clear: true,
+                save: true
+            }
+        }
+    });
+    /***
+     * Creating a new budget item
+     */
+    const buildFirstStageRow = () => {
+        // output table with input fields 
+        let html = `
+                <tr>
+                    <td class="name-field">
+                        <div class="input-field col s6">
+                            <input placeholder="Budget Name" id="settings_budget_name" type="text" class="validate">
+                            <label for="settings_budget_name">Budget Name</label>
+                        </div>
+                    </td>
+                    <td class="colour-cell">
+                        <div class="color-picker"></div>
+                    </td>
+                    <td class="icon-cell">
+                        <a class="waves-effect waves-light btn-small btn-flat btn-settings-edit" data-id="${el.id}"><i class="small fas fa-edit"></i></a>
+                        <a class="waves-effect waves-light btn-small btn-flat btn-settings-delete" data-id="${el.id}"><i class="small fas fa-trash-alt"></i></a>
+                    </td>
+                </tr>
+        `
+        // add buttons 
+
+        // return html
+    }
 
     const setUpEventListeners = () => {
         /** Edit and Delete types */
